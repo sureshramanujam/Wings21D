80b862b7e022cc2255c230079106a6c855fffab1
Suresh Ramanujam <sureshramanujam@gmail.com>
2020-05-07 16:43:00 +0530

    a

--- Controllers/TradeChequeCollectionsReportController.cs
+++ Controllers/TradeChequeCollectionsReportController.cs
@@ -33,9 +33,9 @@ namespace Wings21D.Controllers
                     cmd.Connection = con;
                     //DateTime asonDate = DateTime.Parse(asAtDate);
 
-                    cmd.CommandText = "select DocumentNo, Format(TransactionDate,'dd-MMM-yyyy') As 'CollectionDate', CustomerName" +
+                    cmd.CommandText = "select DocumentNo, Format(TransactionDate,'dd-MMM-yyyy') As 'CollectionDate', CustomerName, " +
                                       "Amount, ChequeNumber, Format(ChequeDate,'dd-MMM-yyyy') As 'ChequeDate', " +
-                                      "RTRIM(ISNULL(AgainstInvoiceNumber,'')) As AgainstInvoiceNumber, " +
+                                      "RTRIM(ISNULL(AgainstInvoiceNumber,'')) As AgainstInvoiceNumber,  TransactionRemarks, " +
                                       "CASE WHEN DownloadedFlag > 0 THEN '1' ELSE '0' END As DownloadedFlag, Username " +
                                       "From ChequeCollections_Table " +
                                       "Where Convert(varchar,TransactionDate,23)>='" + fromDate + "' And " +
