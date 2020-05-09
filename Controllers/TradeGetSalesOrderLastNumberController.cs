829587be2da2b5135a90ef38252bba06c008d9de
Suresh Ramanujam <sureshramanujam@gmail.com>
2020-05-09 21:11:03 +0530

    a

---
+++ Controllers/TradeGetSalesOrderLastNumberController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using Wings21D.Models;
+using System.Linq;
+
+namespace Wings21D.Controllers
+{
+    public class TradeGetSalesOrderLastNumberController : ApiController
+    {
+
+        // GET api/<controller>
+        public HttpResponseMessage Get(string dbName, string userName)
+        {
+            SqlConnection con = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=" + dbName + @";Data Source=localhost\SQLEXPRESS");
+            DataSet ds = new DataSet();
+            List<string> mn = new List<string>();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable LatestSalesOrder = new DataTable();
+
+            if (!String.IsNullOrEmpty(dbName))
+            {
+                try
+                {
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = con;
+
+                    cmd.CommandText = "Select Top 1 DocumentNo From Trade_SalesOrder_Table Where Username='" + userName + "' Order by DocumentNo Desc";
+
+                    da.SelectCommand = cmd;
+                    LatestSalesOrder.TableName = "LatestSalesOrder";
+                    da.Fill(LatestSalesOrder);
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                }
+
+                var returnResponseObject = new
+                {
+                    LatestSalesOrder = LatestSalesOrder
+                };
+
+                var response = Request.CreateResponse(HttpStatusCode.OK, returnResponseObject, MediaTypeHeaderValue.Parse("application/json"));
+                return response;
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+                
+            }
+        }
+    }
+}
