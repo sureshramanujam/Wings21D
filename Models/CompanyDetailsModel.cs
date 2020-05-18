5e09280881c8cfc7f785fe04ae7dda07ffbb7683
Suresh Ramanujam <sureshramanujam@gmail.com>
2020-05-18 11:45:55 +0530

    a

---
+++ Models/CompanyDetailsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wings21D.Models
+{
+    public class CompanyDetails
+    {   
+        public string CompanyDatabaseName { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyLegaName { get; set; }
+        public string CompanyTradeName { get; set; }
+        public string CompanyAddressLine1 { get; set; }
+        public string CompanyAddressLine2 { get; set; }
+        public string CompanyAddressLine3 { get; set; }
+        public string CompanyCity { get; set; }
+        public string CompanyState { get; set; }
+        public string CompanyCountry { get; set; }
+        public string CompanyPINCode { get; set; }
+        public string CompanyFYStartDate { get; set; }
+        public string CompanyFYEndDate { get; set; }
+    }
+}
